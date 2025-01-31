09a0b8ca523bd8dc66422e928f354c467647b8de
silvarge <sep.non2@gmail.com>
2025-01-31 14:17:17 +0900

    [Init] Spring Project Init

---
+++ src/main/java/org/example/todo_practice/TodoPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.todo_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoPracticeApplication.class, args);
+    }
+
+}
